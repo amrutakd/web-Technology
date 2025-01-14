68e43a68c1f0a1e4b4fbfd0d74538d2ed0c275a2
Amruta Kadam <133789549+amrutakd@users.noreply.github.com>
2025-01-14 21:23:55 +0530

    Delete Resturant directory

--- Resturant/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            link.classList.add("active");
-        });
-    });
-});
